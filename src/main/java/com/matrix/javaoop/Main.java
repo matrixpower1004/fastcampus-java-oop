4190ebb14a9c74d78b1b9952f6ed94de9fa7ba0c
matrixpower1004 <matrixpower1004@gmail.com>
2023-06-13 05:36:11 +0900

    Initialize java + gradle
    패스트캠퍼스 자바 oop 프로젝트 셋업

---
+++ src/main/java/com/matrix/javaoop/Main.java
@@ -0,0 +1,11 @@
+package com.matrix.javaoop;
+
+/**
+ * author         : Jason Lee
+ * date           : 2023-06-13
+ * description    :
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
